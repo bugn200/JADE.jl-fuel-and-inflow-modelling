af92657014a0267be0ad9177da7faed6734f4898
Ba Trieu Minh Nguyen <bugn200@UoA.auckland.ac.nz>
2023-09-27 13:22:27 +1300

    Using greater penalty coefficient for calculation

--- src/model.jl
+++ src/model.jl
@@ -658,7 +658,7 @@ function JADEsddp(d::JADEData, optimizer=nothing)
                 )
             end
             # Cost function includes terminal values added
-            SDDP.@stageobjective(md, immediate_cost / scale_obj + terminalcost + fuel_stockpile.out + 1500 * step[1] + 2000 * step[2] + 2500 * step[3])
+            SDDP.@stageobjective(md, immediate_cost / scale_obj + terminalcost + fuel_stockpile.out + 15000 * step[1] + 20000 * step[2] + 25000 * step[3])
         end
     end
 
