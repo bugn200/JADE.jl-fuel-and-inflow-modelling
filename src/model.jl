6b9859092b545c8c3e8a2e15f66dac1e3fe12574
Ba Trieu Minh Nguyen <bugn200@UoA.auckland.ac.nz>
2023-09-27 16:00:26 +1300

    Lower coefficient for step generator due to different penalty function implemented

--- src/model.jl
+++ src/model.jl
@@ -639,7 +639,7 @@ function JADEsddp(d::JADEData, optimizer=nothing)
 
         if stage < number_of_wks || !d.rundata.use_terminal_mwvs
             # Stage cost function not including terminal water value
-            SDDP.@stageobjective(md, immediate_cost / scale_obj+ 0.01*fuel_stockpile.out + 250000 * step[1] + 300000 * step[2] + 350000 * step[3])
+            SDDP.@stageobjective(md, immediate_cost / scale_obj+ 0.01*fuel_stockpile.out + 2500 * step[1] + 3000 * step[2] + 3500 * step[3])
         else
             # Convert stored water in Mm³ to MWh
             JuMP.@expression(
@@ -658,7 +658,7 @@ function JADEsddp(d::JADEData, optimizer=nothing)
                 )
             end
             # Cost function includes terminal values added
-            SDDP.@stageobjective(md, immediate_cost / scale_obj + terminalcost + 0.01*fuel_stockpile.out + 250000 * step[1] + 300000 * step[2] + 350000 * step[3])
+            SDDP.@stageobjective(md, immediate_cost / scale_obj + terminalcost + 0.01*fuel_stockpile.out + 2500 * step[1] + 3000 * step[2] + 3500 * step[3])
         end
     end
 
